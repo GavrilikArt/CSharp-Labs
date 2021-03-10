e687bca8fd6cf901dea0aa60a38b2882fbe83969
GavrilikArt <gavrilik.art@gmail.com>
2021-03-10 09:00:38 +0300

    Added lab2.1.task11

---
+++ Lab2.1.task11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab2.1.task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            string[] words = Console.ReadLine().Split(' ');
+            foreach (string word in words)
+            {
+                if (Char.IsPunctuation(word[word.Length - 1]))
+                {
+                    Console.Write(word[word.Length - 1]);
+                }
+                Console.Write("{0} ", word);
+            }
+            Console.Write("\nDone");
+        }
+    }
+}
+
+
+
+
