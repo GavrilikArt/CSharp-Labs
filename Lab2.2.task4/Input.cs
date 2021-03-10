cae18257ecb1c07874326c8accc788779a45303a
GavrilikArt <gavrilik.art@gmail.com>
2021-03-10 09:33:03 +0300

    Added Lab2.2.task4

---
+++ Lab2.2.task4/Input.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Task4
+{
+    public class Input
+    {
+        public static ulong ReadUlong()
+        {
+            ulong number;
+            while (!UInt64.TryParse(Console.ReadLine(), out number) || number == 0)
+            {
+                Console.Write("Wrong format, enter the correct number.\n");
+            }
+            return number;
+        }
+    }
+}
---
+++ Lab2.2.task4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static ulong PowersBeforeTheNum(ulong number)
+        {
+            ulong amount = 0;
+            ulong pow = 2;
+            for (int i = 1; i < 64; i++)
+            {
+                amount += number / pow;
+                pow *= 2;
+                if (number == 0)
+                {
+                    break;
+                }
+            }
+            return amount;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first natural number:");
+            ulong a = Input.ReadUlong();
+            Console.WriteLine("Enter second natural number:");
+            ulong b = Input.ReadUlong();
+            while (b < a)
+            {
+                Console.WriteLine("The first number must be less than the second!");
+                b = Input.ReadUlong();
+            }
+            Console.WriteLine("Answer is: {0}", PowersBeforeTheNum(b) - PowersBeforeTheNum(a - 1));
+        }
+    }
+}
