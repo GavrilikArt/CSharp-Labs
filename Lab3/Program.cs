4d591da46d7b48fbd08eaaa1840f9cc9bb031bd4
GavrilikArt <gavrilik.art@gmail.com>
2021-03-18 19:51:55 +0300

    minor additions

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -9,34 +9,8 @@ namespace Lab3
     {
         public static void Main(string[] args)
         {
-            /*
-            List<Toyota> list = new List<Toyota>();
-            Car what = new Car(color: "green", material: "iron", transmission: "manual", power: 12,
-                maxSpeed: 12, priceInDollars: 1222, age: 12);
-            Toyota shit = new Toyota();
-            Toyota damn = new Toyota(color: "green", material: "iron", transmission: "manual", model: "shit", power: 13,
-                maxSpeed: 12, countryOfManufacturing: "Nikita", priceInDollars: 1222, age: 5);
-            Toyota toyota = new Toyota(color: "green", material: "iron", transmission: "manual", model: "shit", power: 13,
-                maxSpeed: 12, countryOfManufacturing: "Nikita", priceInDollars: 1222, age: 11);
-            Toyota fu = new Toyota(color: "green", material: "iron", transmission: "manual", model: "shit", power: 13,
-                maxSpeed: 12, countryOfManufacturing: "Nikita", priceInDollars: 1222, age: 10);
-            Console.WriteLine($"{fu.Age}");
-            list.Add(fu);
-            list.Add(toyota);
-            list.Add(damn);
-            Console.WriteLine($"{list.Count}");
-            list.Sort((Toyota a, Toyota b) => a.Age - b.Age);
-            shit = fu;
-            what++;
-            Console.WriteLine($"{fu.Id}");
-            Console.WriteLine(fu);
-            Console.WriteLine(what);
-            shit.GetMinimunInfo();
-            foreach (var i in list)
-            {
-                Console.WriteLine($"age is {i.Id}");
-            }
-            */
+            Toyota toyota = new Toyota(color: "green", material: "iron", transmission: "manual", model: "camry", power: 13,
+                maxSpeed: 12, countryOfManufacturing: "Japan", priceInDollars: 1222, age: 11);
             Vehicle vehicle = new Vehicle(numOfWheels: 1, material: "steel", age: 2, color: "blue");
         }
     }
