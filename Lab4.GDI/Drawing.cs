d15767910940abda941888f79bd50cdcfa257b2c
GavrilikArt <gavrilik.art@gmail.com>
2021-03-24 10:06:21 +0300

    Added Lab4.GDI

---
+++ Lab4.GDI/Drawing.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+
+namespace Lab4.GDI
+{
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Support such commands: fillellips, fillrectangle");
+            Console.WriteLine("Support such colours: red, white");
+            bool wantToDraw = true;
+            do {
+
+                bool wasDrawn;
+                do {
+                    wasDrawn = false;
+                    Console.WriteLine("Enter command, for instance fillellips:red:10:10:100:200");
+                    string command = Console.ReadLine();
+                    command = command.Replace(" ", "");
+                    command = command.ToLower();
+                    string[] parsedCommand = command.Split(':');
+                    if (parsedCommand[0] == "fillellips") {
+                        try {
+                            Color color = StringToColor(parsedCommand[1]);
+                            int x = Convert.ToInt32(parsedCommand[2]);
+                            int y = Convert.ToInt32(parsedCommand[3]);
+                            int width = Convert.ToInt32(parsedCommand[4]);
+                            int height = Convert.ToInt32(parsedCommand[5]);  
+                            Drawing.FillEllips(color, x, y, width, height);
+                            wasDrawn = true;
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine($"Can't work with such command: {ex.Message}");
+                        }
+                    }
+                    else if (parsedCommand[0] == "fillrectangle") {
+                        try {
+                            Color color = StringToColor(parsedCommand[1]);
+                            int x = Convert.ToInt32(parsedCommand[2]);
+                            int y = Convert.ToInt32(parsedCommand[3]);
+                            int width = Convert.ToInt32(parsedCommand[4]);
+                            int height = Convert.ToInt32(parsedCommand[5]);
+                            Drawing.FillRectangle(color, x, y, width, height);
+                            wasDrawn = true;
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine($"Can't work with such command: {ex.Message}");
+                        }
+                    }
+                    else {
+                        Console.WriteLine($"There is no such command {parsedCommand[0]}");
+                    }
+                } while (!wasDrawn);
+                Console.WriteLine("Want to continue? (hit escape to leave, any other - stay): ");
+                ConsoleKeyInfo proposal = Console.ReadKey();
+                if(proposal.Key == ConsoleKey.Escape) {
+                    wantToDraw = false;
+                }
+            } while (wantToDraw);
+        }
+        static Color StringToColor(string colorStr) {
+            Color color;
+            if(colorStr == "red") {
+                color = Color.Red;
+            } else if(colorStr == "white") {
+                color = Color.White;
+            }
+            else {
+                throw new Exception($"No existing color {colorStr}");
+            }
+            return color;
+        }
+    }
+}
---
+++ Lab4.GDI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Lab4.GDI
+{
+    static class Drawing {
+        [DllImport("User32.dll")]
+        static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("User32.dll")]
+        static extern int ReleaseDC(IntPtr hwnd);
+        static public void FillEllips(Color color, int x, int y, int width, int height) {
+            SolidBrush brush= new SolidBrush(color);
+            IntPtr desktopDC = GetDC(IntPtr.Zero);
+            Graphics g = Graphics.FromHdc(desktopDC);
+            g.FillEllipse(brush, x, y, width, height);
+            g.Dispose();
+            ReleaseDC(desktopDC);
+        }
+        static public void FillRectangle(Color color, int x, int y, int width, int height) {
+            SolidBrush brush = new SolidBrush(color);
+            IntPtr desktopDC = GetDC(IntPtr.Zero);
+            Graphics g = Graphics.FromHdc(desktopDC);
+            g.FillRectangle(brush, x, y, width, height);
+            g.Dispose();
+            ReleaseDC(desktopDC);
+        }
+    }
+}
