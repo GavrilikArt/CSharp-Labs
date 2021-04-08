57bab5d9333a9f8e330579ec28163b455bc48fcc
GavrilikArt <gavrilik.art@gmail.com>
2021-04-08 19:00:42 +0300

    Added Lab 7

---
+++ Lab7/Fraction.cs
@@ -0,0 +1,357 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Lab7
+{
+    class Fraction : IComparable<Fraction>, IFormattable, IConvertible, ICloneable
+    {
+        public long Numerator { get => numerator; set => SetNumerator(value); }
+        long numerator;
+        public long Denominator { get => denominator; set => SetDenominator(value); }
+        long denominator;
+
+        public Fraction(long numerator, long denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public Fraction(long numerator)
+        {
+            Numerator = numerator;
+            Denominator = 1;
+        }
+
+        public Fraction(double fraction)
+        {
+            Fraction f = Parse(fraction.ToString());
+            Numerator = f.Numerator;
+            Denominator = f.Denominator;
+        }
+
+        void SetNumerator(long numerator)
+        {
+            if(denominator != 0 && numerator != 0)
+            {
+                long gcd = GCD(Math.Abs(numerator), denominator);
+                numerator /= gcd;
+                denominator /= gcd;
+            }
+            this.numerator = numerator;
+        }
+
+        void SetDenominator(long denominator)
+        {
+            if(denominator == 0)
+            {
+                throw new ArgumentException("Denominator can't be zero.");
+            }
+
+            if(denominator < 0)
+            {
+                denominator *= -1;
+                numerator *= -1;
+            }
+
+            if(numerator != 0)
+            {
+                long gcd = GCD(Math.Abs(numerator), denominator);
+                numerator /= gcd;
+                denominator /= gcd;
+            }
+            this.denominator = denominator;
+        }
+
+        public static Fraction Parse(string s)
+        {
+            if(TryParse(s, out Fraction fraction))
+            {
+                return fraction;
+            }
+            else
+            {
+                throw new FormatException("Input string was not in a correct format.");
+            }
+        }
+
+        public static bool TryParse(string s, out Fraction fraction)
+        {
+            fraction = null;
+            Regex pattern1 = new Regex(@"^(-?\d+)/(\d+)$");
+            Regex pattern2 = new Regex(@"^(-?\d+)$");
+            Regex pattern3 = new Regex(@"^(-?\d+)\((\d+)/(\d+)\)$"); ;
+            Regex pattern4 = new Regex(@"^(-?\d+)[,|\.](\d+)$");
+            if(pattern1.IsMatch(s))
+            {
+                Match match = pattern1.Match(s);
+                fraction = new Fraction(long.Parse(match.Groups[1].Value),
+                                        long.Parse(match.Groups[2].Value));
+                return true;
+            }
+            if(pattern2.IsMatch(s))
+            {
+                Match match = pattern2.Match(s);
+                fraction = new Fraction(long.Parse(match.Groups[1].Value));
+                return true;
+            }
+            if(pattern3.IsMatch(s))
+            {
+                Match match = pattern3.Match(s);
+                long integral = long.Parse(match.Groups[1].Value);
+                int sign = integral > 0 ? 1 : -1;
+                long numerator = long.Parse(match.Groups[2].Value);
+                long denominator = long.Parse(match.Groups[3].Value);
+                fraction = new Fraction((Math.Abs(integral) * denominator + numerator) * sign,
+                                        denominator);
+                return true;
+            }
+            if(pattern4.IsMatch(s))
+            {
+                Match match = pattern4.Match(s);
+                long integral = long.Parse(match.Groups[1].Value);
+                int sign = integral > 0 ? 1 : -1;
+                string floating = match.Groups[2].Value;
+                long denominator = BinPow(10, floating.Length);
+                fraction = new Fraction((Math.Abs(integral) * denominator + long.Parse(floating))
+                                        * sign,
+                                        denominator);
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString() =>
+            ToString("F");
+
+        public string ToString(string format) =>
+            ToString(format, null);
+
+        public string ToString(string format, IFormatProvider provider)
+        {
+            if(string.IsNullOrEmpty(format))
+            {
+                format = "F";
+            }
+            if(format == "IF")
+            {
+                if(Math.Abs(numerator) > denominator && denominator != 1)
+                {
+                    long integral = numerator / denominator;
+                    return $"{integral}({Math.Abs(numerator) % denominator}/{denominator})";
+                }
+                else
+                {
+                    format = "I";
+                }
+            }
+            if(format == "I")
+            {
+                if(Math.Abs(numerator) > denominator)
+                {
+                    long integral = numerator / denominator;
+                    return integral.ToString();
+                }
+                else
+                {
+                    format = "F";
+                }
+            }
+            if(format == "F")
+            {
+                return $"{numerator}/{denominator}";
+            }
+            else if(format == "D")
+            {
+                return GetDoubleValue().ToString();
+            }
+            else if(new Regex(@"D\d*").IsMatch(format))
+            {
+                return GetDoubleValue().ToString("F" + format.Substring(1));
+            }
+            else
+            {
+                throw new FormatException($"The \"{format}\" format is not supported.");
+            }
+        }
+
+        public int CompareTo(Fraction fraction)
+        {
+            long lcm = denominator * fraction.Denominator /
+                       GCD(Math.Abs(denominator), fraction.Denominator);
+
+            long n1 = numerator * (lcm / denominator);
+            long n2 = fraction.Numerator * (lcm / fraction.Denominator);
+            if(n1 > n2)
+                return 1;
+            else if(n1 < n2)
+                return -1;
+            return 0;
+        }
+
+        public object Clone()
+        {
+            return new Fraction(numerator, denominator);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Fraction fraction &&
+                   CompareTo(fraction) == 0;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -671859081;
+            hashCode = hashCode * -1521134295 + numerator.GetHashCode();
+            hashCode = hashCode * -1521134295 + denominator.GetHashCode();
+            return hashCode;
+        }
+
+        static long GCD(long a, long b)
+        {
+            if(a > b)
+            {
+                (a, b) = (b, a);
+            }
+            while(b != 0)
+            {
+                a %= b;
+                (a, b) = (b, a);
+            }
+            return a;
+        }
+
+        static long BinPow(long a, long n)
+        {
+            if(n == 0)
+                return 1;
+            if(n % 2 == 1)
+                return a * BinPow(a, n - 1);
+
+            long b = BinPow(a, n / 2);
+            return b * b;
+        }
+
+        public class DecreaseComparer : IComparer<Fraction>
+        {
+            public int Compare(Fraction a, Fraction b)
+            {
+                return b.CompareTo(a);
+            }
+        }
+
+        double GetDoubleValue() =>
+            (double)numerator / denominator;
+
+        public static explicit operator int(Fraction f) =>
+            f.ToInt32(null);
+
+        public static explicit operator long(Fraction f) =>
+            f.ToInt64(null);
+
+        public static explicit operator float(Fraction f) =>
+            f.ToSingle(null);
+
+        public static explicit operator double(Fraction f) =>
+            f.ToDouble(null);
+
+        public static explicit operator decimal(Fraction f) =>
+            f.ToDecimal(null);
+
+        public TypeCode GetTypeCode() =>
+            TypeCode.Object;
+
+        public bool ToBoolean(IFormatProvider provider) =>
+            Numerator != 0;
+
+        public char ToChar(IFormatProvider provider) =>
+            Convert.ToChar(GetDoubleValue(), provider);
+
+        public sbyte ToSByte(IFormatProvider provider) =>
+            Convert.ToSByte(GetDoubleValue(), provider);
+
+        public byte ToByte(IFormatProvider provider) =>
+            Convert.ToByte(GetDoubleValue(), provider);
+
+        public short ToInt16(IFormatProvider provider) =>
+            Convert.ToInt16(GetDoubleValue(), provider);
+
+        public ushort ToUInt16(IFormatProvider provider) =>
+            Convert.ToUInt16(GetDoubleValue(), provider);
+
+        public int ToInt32(IFormatProvider provider) =>
+            Convert.ToInt32(GetDoubleValue(), provider);
+
+        public uint ToUInt32(IFormatProvider provider) =>
+            Convert.ToUInt32(GetDoubleValue(), provider);
+
+        public long ToInt64(IFormatProvider provider) =>
+            Convert.ToInt64(GetDoubleValue(), provider);
+
+        public ulong ToUInt64(IFormatProvider provider) =>
+            Convert.ToUInt64(GetDoubleValue(), provider);
+
+        public float ToSingle(IFormatProvider provider) =>
+            Convert.ToSingle(GetDoubleValue(), provider);
+
+        public double ToDouble(IFormatProvider provider) =>
+            GetDoubleValue();
+
+        public decimal ToDecimal(IFormatProvider provider) =>
+            Convert.ToDecimal(GetDoubleValue(), provider);
+
+        public DateTime ToDateTime(IFormatProvider provider) =>
+            Convert.ToDateTime(GetDoubleValue(), provider);
+
+        public string ToString(IFormatProvider provider) =>
+            ToString("F", provider);
+
+        public object ToType(Type conversionType, IFormatProvider provider) =>
+            Convert.ChangeType(GetDoubleValue(), conversionType);
+
+
+        public static bool operator >(Fraction a, Fraction b) =>
+            a.CompareTo(b) == 1;
+
+        public static bool operator <(Fraction a, Fraction b) =>
+            a.CompareTo(b) == -1;
+
+        public static bool operator >=(Fraction a, Fraction b) =>
+            a.CompareTo(b) != -1;
+
+        public static bool operator <=(Fraction a, Fraction b) =>
+            a.CompareTo(b) != 1;
+
+        public static bool operator ==(Fraction a, Fraction b) =>
+            a.CompareTo(b) == 0;
+
+        public static bool operator !=(Fraction a, Fraction b) =>
+            a.CompareTo(b) != 0;
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            long lcm = a.Denominator * b.Denominator /
+                      GCD(Math.Abs(a.Denominator), Math.Abs(b.Denominator));
+
+            long n1 = a.Numerator * (lcm / a.Denominator);
+            long n2 = b.Numerator * (lcm / b.Denominator);
+            return new Fraction(n1 + n2, lcm);
+        }
+
+        public static Fraction operator -(Fraction a) =>
+            a * -1;
+
+        public static Fraction operator -(Fraction a, Fraction b) =>
+            a + (-b);
+
+        public static Fraction operator *(Fraction a, Fraction b) =>
+            new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+
+        public static Fraction operator *(Fraction a, long b) =>
+            new Fraction(a.Numerator * b, a.Denominator);
+
+        public static Fraction operator /(Fraction a, Fraction b) =>
+            new Fraction(a.Numerator * b.Denominator, a.Denominator * b.Numerator); 
+    }
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fraction> fractions = new List<Fraction>()
+            {
+                new Fraction(0.25),
+                new Fraction(2),
+                new Fraction(2, 4)
+                //Creating fractions with different constructor types
+            };
+
+            fractions = fractions.Concat(new List<Fraction>()
+            {
+                Fraction.Parse("1/3"),
+                Fraction.Parse("3"),
+                Fraction.Parse("-2(1/4)"),
+                Fraction.Parse("-0.75")
+                //Parsing from different string formats
+            }).ToList();
+            
+            Console.WriteLine("Testing TryParse");
+            Fraction testing;
+            Console.WriteLine(Fraction.TryParse("Not a fraction", out testing));
+            Console.WriteLine(Fraction.TryParse("27/54", out testing));
+            Console.WriteLine(Fraction.TryParse("2(2/4)", out testing));
+            Console.WriteLine(testing);
+            //TryParse function
+
+            fractions = fractions.Concat(new List<Fraction>()
+            {
+                new Fraction(3, 4) + new Fraction(2, 3),
+                new Fraction(5, 6) - new Fraction(1, 6),
+                new Fraction(1, 9) * new Fraction(9, 4),
+                new Fraction(2, 3) / new Fraction(3, 2),
+                //Math operations
+            }).ToList();
+
+            fractions.Add(new Fraction(7, 8).Clone() as Fraction);
+            //Cloning fraction
+
+            Console.WriteLine("____________________");
+            Console.WriteLine(new Fraction(3, 4) > new Fraction(2, 3));
+            Console.WriteLine(new Fraction(5, 6) <= new Fraction(1, 6));
+            Console.WriteLine(new Fraction(1, 2) == new Fraction(2, 4));
+            Console.WriteLine(new Fraction(3, 4) != new Fraction(6, 8));
+            //Comparison operators
+
+            Console.WriteLine("____________________");
+            testing = new Fraction(-5, 2);
+            Console.WriteLine("{0:F}", testing);
+            Console.WriteLine("{0:IF}", testing);
+            Console.WriteLine("{0:I}", testing);
+            Console.WriteLine("{0:D}", testing);
+            Console.WriteLine("{0:D5}", testing);
+            //Converting to string in different formats and IFormattible implementation
+            Console.WriteLine("____________________");
+            Console.WriteLine("//Converting to string in different formats and IFormattible implementation");
+            Console.WriteLine((int)testing);
+            Console.WriteLine((double)testing);
+            Console.WriteLine(Convert.ToBoolean(testing));
+            Console.WriteLine(Convert.ToString(testing));
+            //Conversion operators and implementation of IConvertible
+
+            fractions.Sort();
+            fractions.Sort(new Fraction.DecreaseComparer());
+            fractions.Sort((x, y) => y.CompareTo(x));
+            //Sorting by using IComparable interface, default Decrease comparer and CompareTo
+            //function
+
+            Console.WriteLine("____________________");
+            fractions.ForEach(x => Console.WriteLine("{0:IF}", x));
+
+            Console.WriteLine("____________________");
+            try
+            {
+                new Fraction(2, 0);
+                //Division by zero exception
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }    
+
+            try
+            {
+                Fraction.Parse("Not a fraction");
+                //Trying to parse incorrect string exception
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                testing.ToString("Unsupported format");
+                //Unsupported ToString() format exception
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
